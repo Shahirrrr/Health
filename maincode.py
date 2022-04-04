111df7490925e4b8d59142fcf108a72ea628fce2
Shahirrrr <86062534+Shahirrrr@users.noreply.github.com>
2022-04-04 16:46:14 +0800

    Update maincode.py

--- maincode.py
+++ maincode.py
@@ -13,7 +13,7 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import classification_report
 
 st.header("Welcome to my Streamlit [Machine Learning]")
-st.write("In this WebApp, I will demonstrate 3 types of Machine Learning which are k-Nearest Neighbors, Support Vector Machine (SVM) and Random Forest)
+st.header("In this WebApp, I will demonstrate 3 types of Machine Learning which are k-Nearest Neighbors, Support Vector Machine (SVM) and Random Forest")
 readme = st.checkbox("More Information")
 if readme:
     st.write("""
